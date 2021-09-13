06d573e3c2ca1964bb4f2e18e92cf2fa0de43266
cslee <cs.lee@kaist.ac.kr>
2021-09-13 21:56:04 +0900

    발표자료 및 그래프

--- Movie/utils.py
+++ Movie/utils.py
@@ -12,7 +12,7 @@ import numpy as np
 import random
 import time
 import datetime
-
+import itertools
 
 """
     purpose : BERT 모델에 대한 Tokenizer 세팅
@@ -343,7 +343,7 @@ def run_test(model, test_dataloader, device):
         # 로스 구함
         logits = outputs[0]
 
-        # CPU로 데이터 이동
+        # CPU로 데TensorDataset이터 이동
         logits = logits.detach().cpu().numpy()
         label_ids = b_labels.to('cpu').numpy()
         
@@ -396,13 +396,13 @@ def convert_input_data(tokenizer, sentences):
     output :
         - logits
 """ 
-def test_sentences(model, device, tokenizer, sentences):
+def test_sentence_unit(model, device, tokenizer, sentence):
         
     # 평가모드로 변경
     model.eval()
 
     # 문장을 입력 데이터로 변환
-    inputs, masks = convert_input_data(tokenizer, sentences)
+    inputs, masks = convert_input_data(tokenizer, sentence)
 
     # 데이터를 GPU에 넣음
     b_input_ids = inputs.to(device)
@@ -422,6 +422,58 @@ def test_sentences(model, device, tokenizer, sentences):
     logits = logits.detach().cpu().numpy()
 
     return logits
+
+
+def test_sentence_many(model, device, tokenizer, sentences):
+    start_time = time.time()
+    
+    # 출력된 label 리스트
+    label_list = list() 
+    
+    # 평가모드로 변경
+    model.eval()
+
+    # 문장을 입력 데이터로 변환
+    inputs, masks = convert_input_data(tokenizer, sentences)
+
+    # 데이터를 GPU에 넣음
+    b_input_ids = inputs.to(device)
+    b_input_mask = masks.to(device)
+    
+    test_data = TensorDataset(b_input_ids, b_input_mask)
+    test_dataloader = DataLoader(test_data, batch_size=32)
+    
+    # 데이터로더에서 배치만큼 반복하여 가져옴
+    for step, batch in enumerate(test_dataloader):
+        # 경과 정보 표시
+        if step % 100 == 0 and not step == 0:
+            elapsed = format_time(time.time() - start_time)
+            print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(test_dataloader), elapsed))
+
+        # 배치를 GPU에 넣음
+        batch = tuple(b.to(device) for b in batch)
+
+        # 배치에서 데이터 추출
+        b_input_ids, b_input_mask = batch
+    
+        # 그래디언트 계산 안함
+        with torch.no_grad():     
+            # Forward 수행
+            outputs = model(b_input_ids, 
+                            token_type_ids=None, 
+                            attention_mask=b_input_mask)
+
+        # 로스 구함
+        logits = outputs[0]
+
+        # CPU로 데이터 이동
+        preds = logits.detach().cpu().numpy()
+        pred_flat = np.argmax(preds, axis=1).flatten()
+        label_list.append(list(pred_flat))
+    
+    # 이중 리스트를 단일 리스트로 변경
+    result = list(itertools.chain.from_iterable(label_list))    
+    return result
 
 
 def save_checkpoint(state, path, file_name='checkpoint.pth.tar'):
